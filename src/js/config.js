ba3f0c26c34ba5f577c1dd7228c5257baa1f21cf
Stanstinger <stanleynjuguna288@gmail.com>
2023-02-22 01:39:00 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER__PAGE = 10;
+export const KEY = 'd0028845-ba62-461e-8d0e-e37ecccdf0c1';
+export const MODAL_CLOSE_SEC = 2.5;
